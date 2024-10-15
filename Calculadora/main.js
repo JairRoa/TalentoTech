63f97e7cdc586694bf2ebb501fc876bd82801753
JairRoa <jair.roa.calvo@gmail.com>
2024-10-15 10:17:51 -0500

    video 32

--- Calculadora/main.js
+++
@@ -1,9 +0,0 @@
-const botones = document.querySelectorAll(".btn");
-const pantalla = document.querySelector (".pantalla");
-
-botones.forEach(boton =>{
-    boton.addEventListener("click", ()=>{
-        const botonApretado = boton.textConten;
-        pantalla.textContent = botonApretado;
-    });
-})
