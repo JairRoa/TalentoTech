6ba4ee3e4ec106337b48b3aeddf01a034ee4cbbe
JairRoa <jair.roa.calvo@gmail.com>
2024-10-19 11:20:10 -0500

    video 43

---
+++ fibonacci_3/main.js
@@ -0,0 +1,16 @@
+
+function calcularFibo(n) {
+    let fibo = [0, 1];  
+    if (n === 0) {
+        fibo = [0];
+    } else if (n === 1) {
+        fibo = [0, 1];
+    } else {
+        for (let i = 2; i <= n; i++) {  
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+    }
+    return fibo;
+}
+
+console.log(calcularFibo(9));
